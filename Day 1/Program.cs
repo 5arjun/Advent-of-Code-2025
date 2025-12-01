024550d33ae265743c4d714657350bd1c16d9994
Arjun Patel <arjunpat107@gmail.com>
2025-12-01 12:53:54 -0500

    starting func

---
+++ Day 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+int zeroCount = 0;
+int num = 50;
+
+int getDial(int num, string rotation)
+{
+    
+    return num;
+}
+
+public class StreamReaderExample
+{
+    public static void Main(string[] args)
+    {
+        string filePath = "input.txt"; // Replace with your file path
+
+        try
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    Console.WriteLine("yuh");
+
+                }
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine($"Error: The file '{filePath}' was not found.");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"An I/O error occurred: {ex.Message}");
+        }
+    }
+}
