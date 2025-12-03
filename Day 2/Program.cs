8283ef6ea139f2300e045cb180a34d0549fee0cd
Arjun Patel <arjunpat107@gmail.com>
2025-12-03 14:57:18 -0500

    comments

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -1,4 +1,4 @@
-﻿static int GetDigitFromString(long number, int position) // Updated to long
+﻿static int GetDigitFromString(long number, int position) // founf this func online
 {
     // Position is 0-based, where 0 is the leftmost digit.
     // Example: GetDigitFromString(12345, 0) returns 1, GetDigitFromString(12345, 2) returns 3.
@@ -8,7 +8,7 @@
     {
         throw new ArgumentOutOfRangeException(nameof(position), "Position is out of range for the given number.");
     }
-        // Convert the character at the specified position back to an integer.
+    // Convert the character at the specified position back to an integer.
 
     return int.Parse(numberString[position].ToString());
 }
@@ -20,41 +20,41 @@ try
     string fileContents = File.ReadAllText(filePath);
     char tokenComma = ',';
     char tokenDash = '-';
-    string[] ranges = fileContents.Split(tokenComma);
+    string[] ranges = fileContents.Split(tokenComma); //splits the file into ranges first
 
     foreach (string numberRange in ranges)
-{
-    string[] twoNums = numberRange.Split(tokenDash);
-    long lowerRangeLong = long.Parse(twoNums[0]);  
-    long upperRangeLong = long.Parse(twoNums[1]);  
-    
-    for (long i = lowerRangeLong; i <= upperRangeLong; i++)  
     {
-        string numStr = i.ToString();  
-        int length = numStr.Length;
-        
-        // Only even-length numbers can be invalid
-        if (length % 2 != 0) continue;
-        
-        int half = length / 2;
-        bool isInvalid = true;
-        
-        for (int j = 0; j < half; j++)
+        string[] twoNums = numberRange.Split(tokenDash); //then splits into individual numbers
+        long lowerRangeLong = long.Parse(twoNums[0]); //from string to long
+        long upperRangeLong = long.Parse(twoNums[1]);
+
+        for (long i = lowerRangeLong; i <= upperRangeLong; i++)
         {
-            if (GetDigitFromString(i, j) != GetDigitFromString(i, j + half))
+            string numStr = i.ToString();
+            int length = numStr.Length; //gets the length of the int 
+
+            // Only even length numbers can be invalid
+            if (length % 2 != 0) continue;
+
+            int half = length / 2;
+            bool isInvalid = true;
+
+            for (int j = 0; j < half; j++)
             {
-                isInvalid = false;
-                break;
+                if (GetDigitFromString(i, j) != GetDigitFromString(i, j + half)) //if digit in first half isnt equal to digit in second half
+                {
+                    isInvalid = false;
+                    break;
+                }
+            }
+
+            if (isInvalid) //adds it to the total 
+            {
+                total += i;
             }
         }
-        
-        if (isInvalid)
-        {
-            total += i;
-        }
     }
-}
-    Console.WriteLine($"TOTAL IS: {total}");
+    Console.WriteLine($"TOTAL IS: {total} gang");
 }
 catch (FileNotFoundException)
 {
