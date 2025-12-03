793bc579f5181c83b41bf3574ce9397e21e8e5e4
Arjun Patel <arjunpat107@gmail.com>
2025-12-03 11:11:47 -0500

    reading inpiut file

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string filePath = "input.txt"; 
+try
+{
+    string fileContents = File.ReadAllText(filePath);
+    Console.WriteLine("File contents:\n" + fileContents);
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine($"Error: The file '{filePath}' was not found.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
