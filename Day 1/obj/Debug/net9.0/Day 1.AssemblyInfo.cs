de0b4e4ba68b08146d9a1177271a50b1b50f75ad
Arjun Patel <arjunpat107@gmail.com>
2025-12-03 11:07:10 -0500

    day 2 start

--- Day 1/obj/Debug/net9.0/Day 1.AssemblyInfo.cs
+++ Day 1/obj/Debug/net9.0/Day 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b82b4f4205442aa3748734725c66dfb48c04bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b9d4df1daf43ad5fc97911022cdf3a1c258e5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
