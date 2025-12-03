0fe3f92b4db7624d85b09fa4a27f8c18ec0243eb
Arjun Patel <arjunpat107@gmail.com>
2025-12-03 11:30:58 -0500

    splitting by dash

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -6,14 +6,21 @@ try
     string fileContents = File.ReadAllText(filePath);
     //Console.WriteLine("File contents:\n" + fileContents);
 
-    char tokenComma = ','; 
+    char tokenComma = ',';
+    char tokenDash = '-';
     string[] ranges = fileContents.Split(tokenComma);
 
     foreach (string numberRange in ranges)
     {
-        Console.WriteLine(numberRange);
+        //Console.WriteLine(numberRange);
+        string[] twoNums = numberRange.Split(tokenDash);
+        foreach(string number in twoNums)
+        {
+            Console.WriteLine(number);
+        }
+        
     }
-    
+
 }
 catch (FileNotFoundException)
 {
--- Day 2/obj/Debug/net9.0/Day 2.AssemblyInfo.cs
+++ Day 2/obj/Debug/net9.0/Day 2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+793bc579f5181c83b41bf3574ce9397e21e8e5e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13f7c5e57cb86a0cdba5ba671630051f3065db7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
