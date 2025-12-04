4465c04ad88859db7cc57043911ce99affdc3319
Arjun Patel <arjunpat107@gmail.com>
2025-12-04 12:16:22 -0500

    Day 4 read input

---
+++ Day 4/Program.cs
@@ -0,0 +1,16 @@
+﻿string filePath = "input.txt";
+
+try
+{
+    string fileContents = File.ReadAllText(filePath);
+    Console.WriteLine($"{fileContents}");
+}
+
+catch (FileNotFoundException)
+{
+    Console.WriteLine($"Error: The file '{filePath}' was not found.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
