15a342572f987365ea557e82069b68a4faf6aaa0
Arjun Patel <arjunpat107@gmail.com>
2025-12-03 19:27:12 -0500

    day 3 read file

---
+++ Day 3/Program.cs
@@ -0,0 +1,12 @@
+﻿try
+{
+    string fileContents = File.ReadAllText(filePath);
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine($"Error: The file '{filePath}' was not found.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
