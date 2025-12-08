040d97e5eed7f0963356c591ced51d226a223fe1
Arjun Patel <arjunpat107@gmail.com>
2025-12-08 18:28:43 -0500

    adding ranges to a list

---
+++ Day 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+
+
+string filePath = "input.txt";
+string[] lines = File.ReadAllLines(filePath);
+
+bool gettingRanges = true;
+char tokenDash = '-';
+List<(long Min, long Max)> ranges = new List<(long, long)>();
+
+foreach (string line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        gettingRanges = false;
+        continue;
+    }
+    if (gettingRanges)
+    {
+        string[] tokens = line.Split(tokenDash);
+        long start = (long)double.Parse(tokens[0]);
+        long end = (long)double.Parse(tokens[1]);
+
+        ranges.Add((start, end));
+        
+        Console.WriteLine($"{line}");
+    }
+}
