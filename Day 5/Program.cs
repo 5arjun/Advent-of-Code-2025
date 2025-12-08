402fa9efa944e87076ba26e0974cad50520abd30
Arjun Patel <arjunpat107@gmail.com>
2025-12-08 18:33:35 -0500

    counting valid items

--- Day 5/Program.cs
+++ Day 5/Program.cs
@@ -7,6 +7,7 @@ string[] lines = File.ReadAllLines(filePath);
 bool gettingRanges = true;
 char tokenDash = '-';
 List<(long Min, long Max)> ranges = new List<(long, long)>();
+int valid = 0;
 
 foreach (string line in lines)
 {
@@ -22,7 +23,27 @@ foreach (string line in lines)
         long end = (long)double.Parse(tokens[1]);
 
         ranges.Add((start, end));
-        
+
         Console.WriteLine($"{line}");
     }
+    else
+    {
+        long id = (long)double.Parse(line); //food id
+
+        //bool isFresh = false;
+
+        foreach (var range in ranges)//loops thru ranges
+        {
+            if (id >= range.Min && id <= range.Max)
+            {
+                //isFresh = true;
+                valid++;
+                break; // no need to check further ranges
+            }
+        }
+
+        // now isFresh tells you whether this ID was in any range
+
+    }
 }
+Console.WriteLine($"Valid total: {valid}");
