b9733aad737f52ec5634127f8ff6346af4d66cd3
Arjun Patel <arjunpat107@gmail.com>
2025-12-09 19:50:33 -0500

    making the jblist

--- Day 8/Program.cs
+++ Day 8/Program.cs
@@ -1,7 +1,20 @@
 ﻿using System;
-List<JunctionBox> JBList = new List<JunctionBox>();
+List<Connection> allConnections = new List<Connection>(); //hold every single possible connection
 
-public class JunctionBox //for initially reading the points from the file
+
+string[] lines = File.ReadAllLines("input.txt");
+List<JunctionBox> JBList = new List<JunctionBox>();//for initially reading the points from the file
+int idCounter = 0;
+foreach (string line in lines)
+{
+    string[] coords = line.Split(',');
+    JunctionBox box = new JunctionBox{Id = idCounter, X = coords[0], Y = coords[1], Z = coords[2]};
+    JBList.Add(box);
+    idCounter++;
+}
+
+
+public class JunctionBox 
 {
     public int Id { get; set; } // unique ID 0 to n
     public int X { get; set; }
@@ -13,5 +26,5 @@ public class Connection
 {
     public JunctionBox A { get; set; }
     public JunctionBox B { get; set; }
-    public double distance { get; set; }
+    public double Distance { get; set; }
 }
