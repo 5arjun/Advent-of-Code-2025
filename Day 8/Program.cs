ff7dc4a88946cb8742e9183628adb072c0301afb
Arjun Patel <arjunpat107@gmail.com>
2025-12-09 19:34:22 -0500

    Connection class

--- Day 8/Program.cs
+++ Day 8/Program.cs
@@ -1,9 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-public class JunctionBox
+﻿using System;
+List<JunctionBox> JBList = new List<JunctionBox>();
+
+public class JunctionBox //for initially reading the points from the file
 {
     public int Id { get; set; } // unique ID 0 to n
     public int X { get; set; }
     public int Y { get; set; }
     public int Z { get; set; }
+}
+
+public class Connection
+{
+    public JunctionBox A { get; set; }
+    public JunctionBox B { get; set; }
+    public double distance { get; set; }
 }
