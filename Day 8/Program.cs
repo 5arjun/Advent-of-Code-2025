6f320c845db10a59ebb97d7c91018e420dc755b3
Arjun Patel <arjunpat107@gmail.com>
2025-12-09 19:25:43 -0500

    junction box class

--- Day 8/Program.cs
+++ Day 8/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+public class JunctionBox
+{
+    public int Id { get; set; } // unique ID 0 to n
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Z { get; set; }
+}
